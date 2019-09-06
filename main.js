0fc2f27f059da33b0cef1f357d6e92443208553f
神谷友樹 <jetree900@gmail.com>
2019-09-06 23:47:59 +0900

    opacityの変化を可能にした

--- main.js
+++ main.js
@@ -11,12 +11,12 @@
 
       // templateは``で囲むと改行できる
       template:`
-        <div>
+        <li>
           <p>{{color}}</p>
           <input type="range" @input="childMethod" v-model="value" min="0" max="255">
           <p><input type="number" @input="childMethod" v-model="value" min="0" max="255"></p>
           <p>{{value}}</p>
-        </div>
+        </li>
         `,
 
       methods:{
@@ -37,8 +37,9 @@
           red:"0",
           green:"0",
           blue:"0",
+          opacity:"1",
           style:{
-            backgroundColor:"rgb(0,0,0)",
+            backgroundColor:"rgba(0,0,0,1)",
           },
         }
       },
@@ -48,9 +49,14 @@
           console.log(color)
           console.log(this.$data)
           this.$data[color] = value
-          this.style.backgroundColor = 'rgb('+ this.red + ',' + this.green + ',' + this.blue + ')'
+          this.style.backgroundColor = 'rgba('+ this.red + ',' + this.green + ',' + this.blue + ',' + this.opacity +')'
           console.log(this.style.backgroundColor)
-        }
+        },
+        $changeOpacity:function(){
+          console.log(this.opacity)
+          this.style.backgroundColor = 'rgba('+ this.red + ',' + this.green + ',' + this.blue + ',' + this.opacity +')'
+        },
+
       }
     });
   
